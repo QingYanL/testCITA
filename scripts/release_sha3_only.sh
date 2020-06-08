7e489c8aae0dc65de36c5ecf2e47b4c86367b693
liyan@cryptape.com <liyan@cryptape.com>
2020-06-08 17:12:20 +0800

    test travis streamline

---
+++ scripts/release_sha3_only.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cp -r target/install ./docker/release/cita_secp256k1_sha3
+cd ./docker/release
+tar czf cita_secp256k1_sha3.tar.gz cita_secp256k1_sha3
+
+CITA_CLI_REPOSITORY_NAME=hhliyan/cita-cli-2020-test
+DOCKER_USERNAME=hhliyan
+
+docker build . -t "$CITA_REPOSITORY_NAME":"$TRAVIS_TAG"
+
+docker login --username "$DOCKER_USERNAME" --password "$DOCKER_PASSWORD"
+
+docker push "$CITA_REPOSITORY_NAME":"$TRAVIS_TAG"
