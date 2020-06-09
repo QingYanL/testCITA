32a361f3cc84c3dbd2ca82adddac7d62997a3f9a
liyan@cryptape.com <liyan@cryptape.com>
2020-06-09 17:06:36 +0800

    test travis streamline

--- scripts/release_sha3_only.sh
+++ scripts/release_sha3_only.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-cp -r $(TRAVIS_BUILD_DIR)/target/release-cache docker/release/cita_secp256k1_sha3
 cd ./docker/release || return
 tar czf cita_secp256k1_sha3.tar.gz cita_secp256k1_sha3
 
